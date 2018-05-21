cd04640ecdd00124363e3d5af6962cd5fb7d2f32
Luke Leiter <ldlstarwars@gmail.com>
2018-05-21 13:49:42 -0400

    Third Gun Functional
    *1911 now has a functional script
    *still need to fix disappearing controller
    *need to add more targets

---
+++ VR Gun Range/Assets/Scripts/Object Scripts/PistolScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PistolScript : PVR_InteractionController {
+
+    public GameObject barrelEnd;
+    public AudioClip shotClips;
+
+    private float shootingDelay = .2f;
+    private Shoot shootScript;
+    private int pointValue = 50;
+
+    public override void Awake()
+    {
+        base.Awake();
+        shootScript = GetComponent<Shoot>();
+    }
+
+    public override void Start()
+    {
+        base.Start();
+        shootScript.SetupSound(shotClips);
+    }
+
+    public override void Update()
+    {
+        Vector3 lineOrigin = barrelEnd.transform.position;
+        Debug.DrawRay(lineOrigin, barrelEnd.transform.forward * shootScript.range, Color.green);
+
+        if(Controller.GetPressDown(SteamVR_Controller.ButtonMask.Trigger) && shootScript.timer >= shootingDelay)
+        {
+            shootScript.gunSoundAllowed = true;
+            shootScript.ShootGun(barrelEnd, pointValue, shootingDelay);
+        }
+    }
+}
