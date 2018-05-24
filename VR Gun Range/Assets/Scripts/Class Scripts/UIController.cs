7e962e1ce1c3baee15d5cc8737a2c071a38b292a
Luke Leiter <ldlstarwars@gmail.com>
2018-05-24 14:39:01 -0400

    Created Menu
    *have buttons:play, controls, quit
    *script for the buttons
    *when main scene loads theres a lighting problem
    *need to write controls down in text component

---
+++ VR Gun Range/Assets/Scripts/Class Scripts/UIController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour {
+
+    public Button play;
+    public Button controls;
+    public Button back;
+    public Button quit;
+    public Text controllerExplination;
+
+    private void Start()
+    {
+        play.onClick.AddListener(PlayClicked);
+        controls.onClick.AddListener(ControlsClicked);
+        quit.onClick.AddListener(QuitClicked);
+        back.onClick.AddListener(BackClicked);
+
+        controllerExplination.enabled = false;
+    }
+
+    void PlayClicked()
+    {
+        SceneManager.LoadScene("Main", LoadSceneMode.Single);
+    }
+
+    void ControlsClicked()
+    {
+        play.gameObject.SetActive(false);
+        controls.gameObject.SetActive(false);
+        quit.gameObject.SetActive(false);
+
+        back.gameObject.SetActive(true);
+        controllerExplination.enabled = true;
+    }
+
+    void BackClicked()
+    {
+        play.gameObject.SetActive(true);
+        controls.gameObject.SetActive(true);
+        quit.gameObject.SetActive(true);
+
+        back.gameObject.SetActive(false);
+        controllerExplination.enabled = false;
+    }
+
+    void QuitClicked()
+    {
+        Application.Quit();
+    }
+}
